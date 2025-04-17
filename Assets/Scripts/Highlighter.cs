9463da0c2fd0bc6bfe9357df0676f5e3d758cb02
Smithp518 <smithp@colostate.edu>
2025-04-17 13:05:55 -0600

    Adding new Unity project

---
+++ Assets/Scripts/Highlighter.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Highlighter : MonoBehaviour
+{
+    public Material normalMaterial;
+    public Material highlightMaterial;
+    public AudioSource audioSource;
+
+    private MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = normalMaterial;
+
+        var interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable>();
+        interactable.hoverEntered.AddListener(OnHoverEnter);
+        interactable.hoverExited.AddListener(OnHoverExit);
+        interactable.selectEntered.AddListener(OnClick);
+    }
+
+    void OnHoverEnter(HoverEnterEventArgs args)
+    {
+        meshRenderer.material = highlightMaterial;
+    }
+
+    void OnHoverExit(HoverExitEventArgs args)
+    {
+        meshRenderer.material = normalMaterial;
+    }
+
+    void OnClick(SelectEnterEventArgs args)
+    {
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+    }
+}
